65dbd4d39bef3789864d1c8f5a283562677f9e5f
Arthur41k <23fmif.a.paskhalin@std.udu.edu.ua>
2024-04-02 11:54:59 +0300

    В клас Carriage додав зміні speed, EnginePower, color та методи ProcessingEnginer(),Speed(),Color() що їх обробляють.

--- Train/Carriage.cs
+++ Train/Carriage.cs
@@ -22,6 +22,13 @@ namespace Train
 
         public int compartmentsCount { get; set; }
 
+        //Власні зміні
+        double speed { get; set; }
+
+        int EnginePower { get; set; }
+
+        string color { get; set; }
+
         //Конструктор приймає номер,вагу та довжину вагона
         public Carriage(int identifier, string type, double weight, double length, int seatsCount = 0, int compartmentsCount = 0)
         {
@@ -31,6 +38,62 @@ namespace Train
             this.length = length;
             this.seatsCount = seatsCount;
             this.compartmentsCount = compartmentsCount;
+        }
+
+        /// <summary>
+        /// Метод що в залежності від типу потяга визначає силу його двигуна
+        /// </summary>
+        /// <param name="carriages"></param>
+        public void ProcessingEnginer(LinkedList<Carriage> carriages)
+        {
+            Random random = new Random();
+            if (carriages.First?.Value != null && carriages.First.Value.type == "FreightCarriage")
+            {
+                EnginePower = random.Next(80, 160);
+            }
+            else
+            {
+                EnginePower = random.Next(50, 100);
+            }
+        }
+
+        /// <summary>
+        /// Визначає швидкість вагона(а одже і всього потяга) в залежності від ваги та довжини потяга в якому знаходиться цей вагон 
+        /// </summary>
+        /// <param name="carriages"></param>
+        public void Speed(LinkedList<Carriage> carriages)
+        {
+            double AllWeight = 0;
+            double Alllength = 0;
+            foreach (Carriage car in carriages)
+            {
+                AllWeight += car.weight;
+                Alllength += car.length;
+            }
+            speed = (EnginePower - AllWeight * 0.05) - Alllength * 0.01;
+        }
+
+        /// <summary>
+        /// Визначає колір вагона
+        /// </summary>
+        public void Color()
+        {
+            if (type == "FreightCarriage")
+            {
+                color = "Green";
+            }
+            else if (type == "PassengerCarriage")
+            {
+                color = "Blue";
+            }
+            else if (type == "SleepingCarriage")
+            {
+                color = "White";
+            }
+            else if (type == "DiningCarriage")
+            {
+                color = "Red";
+            }
         }
     }
 }
